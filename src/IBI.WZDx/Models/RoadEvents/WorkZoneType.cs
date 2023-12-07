667947d9820694e9c8559252314c067b0263096c
Kuldeep Singh Bamrah <kuldeep.singhbamrah@ibigroup.com>
2023-12-07 13:46:06 -0500

    Add WorkZoneType enum

---
+++ src/IBI.WZDx/Models/RoadEvents/WorkZoneType.cs
@@ -0,0 +1,28 @@
+﻿namespace IBI.WZDx.Models.RoadEvents;
+
+/// <summary>
+/// The type of work zone road event.
+/// </summary>
+public enum WorkZoneType
+{
+    /// <summary>
+    /// The road event is statically placed and is not moving.
+    /// </summary>
+    Static,
+
+    /// <summary>
+    /// The road event is actively moving on the roadway.
+    /// </summary>
+    /// <remarks>
+    /// As opposed to <see cref="PlannedMovingArea"/>, the road event geometry changes at the operation moves. 
+    /// </remarks>
+    Moving,
+
+    /// <summary>
+    /// The planned extent of a moving operation. The active work area will be somewhere within this road event.
+    /// </summary>
+    /// <remarks>
+    /// As opposed to <see cref="Moving"/>, the road event geometry typically does not actively change. 
+    /// </remarks>
+    PlannedMovingArea
+}
